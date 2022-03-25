8c29828dc0bbb4844521b65872555043cdbc5f4a
Martin Macario <9425915+martinmacario@users.noreply.github.com>
2022-03-25 12:40:30 -0300

    added styles to home.

--- pages/404.js
+++ pages/404.js
@@ -11,7 +11,7 @@ const Error10 = () => {
                     <img className="md:hidden" src="https://i.ibb.co/8gTVH2Y/Group-198.png" alt="" />
                 </div>
                 <div className="w-full lg:w-1/2">
-                    <h1 className="py-4 text-3xl lg:text-4xl font-extrabold text-gray-800">Looks like you've found the doorway to the great nothing</h1>
+                    <h1 className="py-4 text-3xl lg:text-4xl font-extrabold text-gray-800">No se que pasa en esta ciudad</h1>
                     <p className="py-4 text-base text-gray-800">The content you’re looking for doesn’t exist. Either it was removed, or you mistyped the link.</p>
                     <p className="py-2 text-base text-gray-800">Sorry about that! But someone has drinked all the wine here.</p>
                     <button className="w-full lg:w-auto my-4 border rounded-md px-1 sm:px-16 py-5 bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50">Go back to Homepage</button>
--- pages/index.tsx
+++ pages/index.tsx
@@ -17,63 +17,6 @@ export default function Home() {
         <CtaAct></CtaAct>
         <Feature></Feature>
         <CtaJoin></CtaJoin>
-        {/* <h1 className="text-6xl font-bold">
-          Bienvenidos al{' '}
-          <a className="text-blue-600" href="https://discord.gg/tVW6e9FH">
-            Cordoverse
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Primera DAO de Córdoba{' '}
-        </p>
-        <p className="mt-3 text-xl">
-          <code className="rounded-md pt-3 font-mono text-lg">
-            cuyo propósito es potenciar el ecosistema crypto de la región fomentando espacios de encuentro, conversación y co-creación #WAGMI
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://twitter.com/Cordoverse"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Twitter &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Seguinos en twitter
-            </p>
-          </a>
-
-          <a
-            href="https://discord.gg/tVW6e9FH"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Discord &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Unite a nuestro discord
-            </p>
-          </a>
-
-          <a
-            href="#"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Eventos &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Segui y participa en nuestros eventos
-            </p>
-          </a>
-
-          <a
-            href="#"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Participa &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Te invitamos a ser parte
-            </p>
-          </a>
-        </div> */}
     </Layout>
   )
 }
